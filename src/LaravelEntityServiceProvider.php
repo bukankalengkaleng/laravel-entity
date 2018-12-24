e05222e2917a614d918c8e5d7db0d417de459b0e
R. Kukuh <rkukuh@gmail.com>
2018-12-24 17:35:59 +0700

    Create service provider

---
+++ src/LaravelEntityServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace BukanKalengKaleng\LaravelEntity;
+
+use Illuminate\Support\ServiceProvider;
+use BukanKalengKaleng\LaravelEntity\Console\Commands\LaravelEntity;
+
+class LaravelEntityServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                EntityMake::class,
+                EntityDelete::class,
+            ]);
+        }
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
